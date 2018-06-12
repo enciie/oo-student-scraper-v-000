eec59e8c07582d60b0b624f84a053d2bb28c7540
Nancy Jo <enciie@gmail.com>
2018-06-12 00:58:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,12 +4,21 @@ class Student
 
   @@all = []
 
+  #use metaprogramming to assign the newly created student attributes 
+  #and values in accordance with the key/value pairs of the hash
+  #Use the #send method to acheive this. 
+  #This method should also add the newly created student to the Student class' @@all array of all students.
   def initialize(student_hash)
-    
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    @@all << self
   end
 
+  ##create_from_collection method should iterate over the array 
+  #of hashes and create a new individual student using each hash. 
   def self.create_from_collection(students_array)
-    
+    student_array.each {|student_hash| Student.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
